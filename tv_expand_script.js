4c6043a6ea22864d0890e49ffaa6327888523c3b
David Johnson <davidj.sba@gmail.com>
2015-09-02 12:20:35 -0400

    throwing up the first files onto my repo

---
+++ tv_expand_script.js
@@ -0,0 +1,355 @@
+
+function init() {
+
+var product = new Object();
+product.Item = new Array();
+
+
+/* Panel 1 */
+/* Panel 1  Product 1*/
+product.Item.push({
+  "Panel":"one",
+  "Image_url" : "images/panel1product1.png", 
+  "Image_width" : null,
+  "Top" : "50px", 
+  "Left" : "10px",
+  "BtnTop" : "41px", 
+  "BtnLeft" : "64px",  
+  "Price" : "$19.99", 
+  "Subhead" : "SAVE 30%",
+  "Title" : "50-Ft. NeverKink Garden Hose",
+  "Desc" : "- Self straightening technology &nbsp;&nbsp;guarantees no kinks or tangles<br/>- Click-Seal resists leaks<br/>- 5/8 in. x 50-ft.<br/>- Lifetime warranty",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com",
+  "Lightbox_Image_Width" : null,
+  "ProductTop" : "50px", 
+  "ProductLeft" : "49px",
+  "LogoImage_Url" : "images/panel1product1-logo.png",
+  "LogoTop" : "-100px",
+  "LogoLeft" : "110px"
+});
+
+/* Panel 1  Product 2*/
+product.Item.push({
+  "Panel":"one",
+  "Image_url" : "images/panel1product2.png",  
+  "Image_width" : null,
+  "Top" : "40px", 
+  "Left" : "120px",
+  "BtnTop" : "48px", 
+  "BtnLeft" : "181px",
+  "Price" : "$99.97", 
+  "Subhead" : "Special Buy",
+  "Title" : "20-Volt Cordless String Trimmer and Blower Combo Kit",
+  "Desc" : "- 12&#8243; string trimmer has auto feeding &nbsp;&nbsp;system<br/>- Sweeper/blower has built-in scraper to &nbsp;&nbsp;loosen wet leaves from surfaces",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : null,
+  "ProductTop" : "30px", 
+  "ProductLeft" : "39px",
+  "LogoImage_Url" : "images/panel1product2-logo.png",
+  "LogoTop" : "-20px",
+  "LogoLeft" : "90px"
+});
+
+/* Panel 1  Product 3*/
+product.Item.push({
+  "Panel":"one",
+  "Image_url" : "images/panel1product3.png",
+  "Image_width" : null,
+  "Top" : "90px", 
+  "Left" : "176px",
+  "BtnTop" : "134px", 
+  "BtnLeft" : "220px",  
+  "Price" : "$14.99", 
+  "Subhead" : "Save 50%",
+  "Title" : "Heavy-Duty Metal Oscillating Sprinkler",
+  "Desc" : "- Covers up to 3,500 sq. ft.<br/>- 17 brass jets resists clogs<br/>- Easy-to-use rotation dial",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "150px",
+  "ProductTop" : "57px", 
+  "ProductLeft" : "29px",
+  "LogoImage_Url" : null,
+  "LogoTop" : null,
+  "LogoLeft" : null
+});
+
+
+/* Panel 2 */
+/* Panel 2  Product 1*/
+product.Item.push({
+  "Panel":"two",
+  "Image_url" : "images/panel2product1.png", 
+  "Image_width" : null,
+  "Top" : "50px", 
+  "Left" : "10px",
+  "BtnTop" : "131px", 
+  "BtnLeft" : "52px", 
+  "Price" : "$14.99", 
+  "Subhead" : "Save 30%",
+  "Title" : "16-Oz. Vacuum Travel Cup",
+  "Desc" : "- Durable stainless steel cup<br/>- Insulated<br/>- Dishwasher safe",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "90px",
+  "ProductTop" : "13px", 
+  "ProductLeft" : "49px",
+  "LogoImage_Url" : null,
+  "LogoTop" : null,
+  "LogoLeft" : null
+});
+
+/* Panel 2  Product 2*/
+product.Item.push({
+  "Panel":"two",
+  "Image_url" : "images/panel2product2.png", 
+  "Image_width" : null,
+  "Top" : "50px", 
+  "Left" : "93px",
+  "BtnTop" : "68px", 
+  "BtnLeft" : "135px", 
+  "Price" : "$199.00", 
+  "Subhead" : "Save $20",
+  "Title" : "21in. 3-in-1 Gas Mower",
+  "Desc" : "- 140-cc Briggs & Stratton engine<br/>- Bag, side discharge or mulch capable",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : null,
+  "ProductTop" : "40px", 
+  "ProductLeft" : "29px",
+  "LogoImage_Url" : "images/panel2product2-logo.png",
+  "LogoTop" : "-100px",
+  "LogoLeft" : "105px"
+});
+
+/* Panel 2  Product 3*/
+product.Item.push({
+  "Panel":"two",
+  "Image_url" : "images/panel2product3.png",
+  "Image_width" : null,
+  "Top" : "40px", 
+  "Left" : "216px",
+  "BtnTop" : "83px", 
+  "BtnLeft" : "240px",  
+  "Price" : "$29.99", 
+  "Subhead" : "Save $5",
+  "Title" : "3-Pc. BBQ Tool Set",
+  "Desc" : "- Stainless steel spatula, fort and tongs",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "80px",
+  "ProductTop" : "20px", 
+  "ProductLeft" : "69px",
+  "LogoImage_Url" : "images/panel2product3-logo.png",
+  "LogoTop" : "10px",
+  "LogoLeft" : "20px"
+});
+
+
+/* Panel 3 */
+/* Panel 3  Product 1*/
+product.Item.push({
+  "Panel":"three",
+  "Image_url" : "images/panel3product1.png",
+  "Image_width" : null,
+  "Top" : "80px", 
+  "Left" : "5px",
+  "BtnTop" : "64px", 
+  "BtnLeft" : "39px",  
+  "Price" : "$17.99", 
+  "Subhead" : "Sale",
+  "Title" : "1.33-Gal. Grass & Weed Killer",
+  "Desc" : "- Provides up to 10 minutes continuous &nbsp;&nbsp;spray<br/>- Easy-to-operate extendable wand<br/>- Refillable container with nested top &nbsp;&nbsp;sprayer",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "110px",
+  "ProductTop" : "27px", 
+  "ProductLeft" : "44px",
+  "LogoImage_Url" : null,
+  "LogoTop" : null,
+  "LogoLeft" : null
+});
+
+/* Panel 3  Product 2*/
+product.Item.push({
+  "Panel":"three",
+  "Image_url" : "images/panel3product2.png", 
+  "Image_width" : null,
+  "Top" : "53px", 
+  "Left" : "97px",
+  "BtnTop" : "63px", 
+  "BtnLeft" : "106px", 
+  "Price" : "$19.99", 
+  "Subhead" : "Save over 30%",
+  "Title" : "Deluxe Sports Chair",
+  "Desc" : "- Sturdy steel frame with 3D mesh for &nbsp;&nbsp;comfort<br/>- Padded armrests<br/>- Built-in cup holder",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "110px",
+  "ProductTop" : "17px", 
+  "ProductLeft" : "49px",
+  "LogoImage_Url" : null,
+  "LogoTop" : null,
+  "LogoLeft" : null
+});
+
+/* Panel 3  Product 3*/
+product.Item.push({
+  "Panel":"three",
+  "Image_url" : "images/panel3product3.png",
+  "Image_width" : null,
+  "Top" : "52px", 
+  "Left" : "190px",
+  "BtnTop" : "82px", 
+  "BtnLeft" : "198px",  
+  "Price" : "$1.99<span style='font-size:18px;'>Each</span>", 
+  "Subhead" : "Save 33%",
+  "Title" : "Solar Path Light",
+  "Desc" : "- Bright white LED illumintaes clear plastic lens",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "40px",
+  "ProductTop" : "30px", 
+  "ProductLeft" : "79px",
+  "LogoImage_Url" : null,
+  "LogoTop" : null,
+  "LogoLeft" : null
+});
+
+/* Panel 3  Product 4*/
+product.Item.push({
+  "Panel":"three",
+  "Image_url" : "images/panel3product4.png", 
+  "Image_width" : null,
+  "Top" : "40px", 
+  "Left" : "236px",
+  "BtnTop" : "104px", 
+  "BtnLeft" : "240px", 
+  "Price" : "$17.99", 
+  "Subhead" : "Save Over 25%",
+  "Title" : "16-In. Oscillating Stand Fan",
+  "Desc" : "- 3-speeds<br/>- Adjustable height with tilting head",
+  "Link_text" : "Shop Now", 
+  "Link_url":"http://google.com", 
+  "Lightbox_Image_Width" : "58px",
+  "ProductTop" : "20px", 
+  "ProductLeft" : "74px",
+  "LogoImage_Url" : null,
+  "LogoTop" : null,
+  "LogoLeft" : null
+});
+
+function lightbox(title) {
+
+  $.each(product.Item, function(index){
+	if(this.Title == title) {
+	  $('#SBA_expand_lightbox_right_content').append('<h2 class="SBA_expand_product_price">' + this.Price + '<h2/>');
+	  $('#SBA_expand_lightbox_right_content').append('<h3 class="SBA_expand_product_Subhead">' + this.Subhead + '<h3/>');
+	  $('#SBA_expand_lightbox_right_content').append('<h1 class="SBA_expand_product_title">' + this.Title + '<h1/>');
+	  $('#SBA_expand_lightbox_right_content').append('<p class="SBA_expand_product_Desc">' + this.Desc + '<p/>');
+	  $('#SBA_expand_lightbox_right_content').append('<a href="' + this.Link_url + '"class="SBA_expand_product_cta">' + this.Link_text + '<a/>');
+	  $('#SBA_expand_lightbox_left').append('<img data-title="'+ this.Title +'" src="' + this.Image_url + '" style="width:' + this.Lightbox_Image_Width + '; top:' + this.ProductTop + '; left:' + this.ProductLeft + '; position:relative;" / ><br/>');
+	  if(this.LogoImage_Url != null){
+		$('#SBA_expand_lightbox_left').append('<img data-title="'+ this.Title +'" src="' + this.LogoImage_Url + '" style="top:' + this.LogoTop + '; left:' + this.LogoLeft + '; position:relative;" / ><br/>');
+	  }
+	  $('#SBA_expand_lightbox').addClass('SBA_expand_show');
+	}
+  });
+}
+
+
+$.each(product.Item, function(index){
+	if (this.Panel == "one") {
+		  $('#one').append('<img src="' + this.Image_url + '" style="width:' + this.Image_width + '; top:' + this.Top + '; left:' + this.Left + ';" / ><br/>');
+		  $('#one').append('<img class="SBA_expand_product" data-title="'+ this.Title +'"src="images/plus-btn.png" style="top:' + this.BtnTop + '; left:' + this.BtnLeft + '; cursor: pointer; " / ><br/>');
+	}
+	else if (this.Panel == "two") {
+		 $('#two').append('<img src="' + this.Image_url + '" style="width:' + this.Image_width + '; top:' + this.Top + '; left:' + this.Left + ';" / ><br/>');
+		 $('#two').append('<img class="SBA_expand_product" data-title="'+ this.Title +'" src="images/plus-btn.png" style="top:' + this.BtnTop + '; left:' + this.BtnLeft + '; cursor: pointer;" / ><br/>');
+	}
+	else if(this.Panel == "three") {
+		  $('#three').append('<img src="' + this.Image_url + '" style="width:' + this.Image_width + '; top:' + this.Top + '; left:' + this.Left + ';" / ><br/>');
+		  $('#three').append('<img class="SBA_expand_product" data-title="'+ this.Title +'" src="images/plus-btn.png" style="top:' + this.BtnTop + '; left:' + this.BtnLeft + '; cursor: pointer;" / ><br/>');
+	}
+});
+
+
+/* Runs function to open Lightbox */
+$('.SBA_expand_product').each(function(index) {
+
+  $(this).on('click', function() {
+	var thisTitle = $(this).data('title');
+	lightbox(thisTitle);
+
+  });
+});
+
+
+/* Lightbox Close Button */
+
+function closeLightbox(){
+	$('#SBA_expand_lightbox').removeClass('SBA_expand_show');
+	$('#SBA_expand_lightbox_left').empty();
+	$('#SBA_expand_lightbox_right_content').empty();
+}
+
+$('#SBA_expand_lightbox_close').click(function(){
+   closeLightbox();
+});
+
+
+/* adds black background to clicked nav btn */
+$('li.menu').click(function(){
+	$('li.menu').removeClass("active");
+	$(this).addClass("active");
+});
+
+
+/* shows and hides panels */
+$('#panel1_btn').click(function(){
+  $('section.block').removeClass('panelShow');
+  $('section#one').addClass('panelShow');
+   closeLightbox();
+});
+
+$('#panel2_btn').click(function(){
+  $('section.block').removeClass('panelShow');
+  $('section#two').addClass('panelShow');
+   closeLightbox();
+});
+
+$('#panel3_btn').click(function(){
+  $('section.block').removeClass('panelShow');
+  $('section#three').addClass('panelShow');
+   closeLightbox();
+});
+
+
+/* disclaimer popup */
+$('#SBA_expand_disclaimer_btn').click(function(){
+  $('#SBA_expand_disclaimer').css('display', 'block');
+});
+$('#SBA_expand_disclaimer_close_btn').click(function(){
+  $('#SBA_expand_disclaimer').css('display', 'none');
+});
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
